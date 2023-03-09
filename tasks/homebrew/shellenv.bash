cc135f31fb73d35791aed7f0fa1db9dbb4c61838
Mizunashi Mana <mizunashi-mana@noreply.git>
2023-03-09 23:56:20 +0900

    Fix for debian

---
+++ tasks/homebrew/shellenv.bash
@@ -0,0 +1,9 @@
+function shellenv() {
+  if [ -d /opt/homebrew ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  elif [ -d "$HOME/.linuxbrew" ]; then
+    eval "$("$HOME/.linuxbrew/bin/brew" shellenv)"
+  elif [ -d /home/linuxbrew/.linuxbrew ]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  fi
+}
