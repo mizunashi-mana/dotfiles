6ab6dc1b0983ac4acecfab406a1f96a73b2ed55a
Mizunashi Mana <contact@mizunashi.work>
2025-04-26 23:11:54 +0900

    Fix vscode extensions

--- nix/home-manager/programs/vscode/default.nix
+++ nix/home-manager/programs/vscode/default.nix
@@ -4,13 +4,16 @@
 
     profiles.default = {
       extensions = [
-        pkgs.vscode-extensions.docker.docker
         pkgs.vscode-extensions.editorconfig.editorconfig
         pkgs.vscode-extensions.github.copilot
         pkgs.vscode-extensions.github.copilot-chat
+        pkgs.vscode-extensions.github.vscode-github-actions
         pkgs.vscode-extensions.jnoortheen.nix-ide
         pkgs.vscode-extensions.ms-azuretools.vscode-docker
         pkgs.vscode-extensions.ms-vscode-remote.remote-containers
+        pkgs.vscode-extensions.ms-vscode.hexeditor
+        pkgs.vscode-extensions.ms-toolsai.jupyter
+        pkgs.vscode-extensions.ms-python.python
       ];
     };
   };
