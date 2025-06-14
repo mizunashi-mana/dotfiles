41ff7d570c28edf35262b861e489ba4925141022
Mizunashi Mana <contact@mizunashi.work>
2025-06-14 20:52:26 +0900

    Add vscode extensions

--- nix/programs/vscode/default.nix
+++ nix/programs/vscode/default.nix
@@ -10,21 +10,22 @@
 
         profiles.default = {
           extensions = [
+            packages.pkgs.vscode-extensions.astro-build.astro-vscode
             packages.pkgs.vscode-extensions.charliermarsh.ruff
             packages.pkgs.vscode-extensions.dbaeumer.vscode-eslint
-            packages.pkgs.vscode-extensions.editorconfig.editorconfig
             packages.pkgs.vscode-extensions.eamodio.gitlens
+            packages.pkgs.vscode-extensions.editorconfig.editorconfig
             packages.pkgs.vscode-extensions.github.copilot
             packages.pkgs.vscode-extensions.github.copilot-chat
             packages.pkgs.vscode-extensions.github.vscode-github-actions
             packages.pkgs.vscode-extensions.jnoortheen.nix-ide
             packages.pkgs.vscode-extensions.ms-azuretools.vscode-docker
-            packages.pkgs.vscode-extensions.ms-vscode-remote.remote-containers
-            packages.pkgs.vscode-extensions.ms-vscode.hexeditor
-            packages.pkgs.vscode-extensions.ms-toolsai.jupyter
-            packages.pkgs.vscode-extensions.ms-python.python
             packages.pkgs.vscode-extensions.ms-kubernetes-tools.vscode-kubernetes-tools
+            packages.pkgs.vscode-extensions.ms-python.python
+            packages.pkgs.vscode-extensions.ms-toolsai.jupyter
+            packages.pkgs.vscode-extensions.ms-vscode.hexeditor
             packages.pkgs.vscode-extensions.ms-vscode.makefile-tools
+            packages.pkgs.vscode-extensions.ms-vscode-remote.remote-containers
             packages.pkgs.vscode-extensions.redhat.vscode-yaml
             packages.pkgs.vscode-extensions.stylelint.vscode-stylelint
           ];
